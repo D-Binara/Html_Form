9241d85acfcfb06bbb4063a69c63b11fb60dc87c
D_Binara <dbkaveesha@gmail.com>
2023-07-08 22:10:08 +0530

    upload

--- htmlform.php
+++ htmlform.php
@@ -2,22 +2,32 @@
 <html>
     <head>
         <link rel="stylesheet" href="style.css">
+        <!--bootstrap CDN link-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <title>Test Form</title>
     </head>
 
     <body>
         <h1>Fill this form</h1>
 
-        <form method="get">
+        <form class ="container" method="get">
             <!-- get user inputs--> 
-            <label for="fname">First Name</lable><br>
-            <input type="text" id="fname" name="fname"><br>
-            <label for="lname">Last Name</lable><br>
-            <input type="text" id="lname" name="lname"><br>
-            <label for="address">Address</lable><br>
-            <input type="text" id="address" name="address"><br>
-            <label for="id">ID</lable><br>
-            <input type="text" id="id" name="id"><br>
+            <div class="form-group">
+                <label for="fname">First Name</lable><br>
+                <input type="text" class="form-control" id="fname" name="fname" placeholder="First Name"><br>
+            </div>
+            <div class="form-group">
+                <label for="lname">Last Name</lable><br>
+                <input type="text"  class="form-control" id="lname" name="lname" placeholder="Last Name"><br>
+            </div>
+            <div class="form-group">
+                <label for="address">Address</lable><br>
+                <input type="text" class="form-control" id="address" name="address" placeholder="Address"><br>
+            </div>
+            <div class="form-group">
+                <label for="id">ID</lable><br>
+                <input type="text" class="form-control" id="id" name="id" placeholder="ID"><br>
+            </div>
             <!-- submit user input--> 
             <input type="submit" value="Submit">
         </form>
@@ -44,6 +54,7 @@
             if(isset($_GET["id"])){
                 $id = $_GET["id"];
                 } 
+   
 
             $servername = "localhost";
             $username = "root";
@@ -60,10 +71,11 @@
             //SQL query  
             $sql = "INSERT INTO user_details (First_Name,Last_Name,address_,ID)
                         VALUES ('$fname', '$lname','$address','$id')";
-                        
-
+            
+            
+            
             if ($conn->query($sql) === TRUE) {
-             echo "New record created successfully";
+             echo "<br>.New record created successfully";
             } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
             }
