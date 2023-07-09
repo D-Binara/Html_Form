cc711a642e0445a5c582c5bd1b3b10edf8c7ef07
D_Binara <dbkaveesha@gmail.com>
2023-07-09 23:55:44 +0530

    update

--- includes/get_user.php
+++ includes/get_user.php
@@ -12,28 +12,22 @@ $username = "root";
 $password = "";
 $dbname = "html_form_db";
 
-$status = true;
-$error = null;
-
-
  // Create connection
  $conn = new mysqli($servername, $username, $password, $dbname);
 
  if ($conn->connect_error) {
-    $error = $conn->connect_error;
+     
  }
 
-//SQL query  
-$sql= "SELECT * FROM user_details  WHERE ID='$id'";
+$sql="SELECT * FROM user_details WHERE ID='$id'";
 
 if ($conn->query($sql) === TRUE) {
     
 } else {
-    $status = false;
-    $error = $conn->error;
+    echo $conn->error;
 }
 
 $conn->close();
 
-header("Location: /HTML_Form/htmlform.php?details=$sql&success2=$status&error2=$error");
+header("Location: /HTML_Form/htmlform.php?details=$sql");
 ?>
